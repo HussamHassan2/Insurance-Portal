fbc4988a64638de45847d41851b2fc68bcc60eb9
HussamHasasn2 <hussamhassan146@gmail.com>
2025-12-25 19:52:10 +0200

    build script

--- build.sh
+++ build.sh
@@ -1,20 +1,14 @@
-echo "Get latest updates..."
-sudo git pull origin main
-echo "Installing dependencies..."
-npm install
 echo "Building the project..."
-ng build --configuration orient
+ng build --configuration orient  --output-hashing=all --base-href http://10.245.2.246:4002/portal/
 echo "Build completed."
 echo "You can find the build output in the 'dist' directory."
 echo "Copy .htaccess file..."
 sudo cp .htaccess dist/insurance-portal/
 echo ".htaccess file copied."
-echo "Build process finished."
 echo "-----------------------------------"
-echo "Starting deployment..."
-sudo rm -rf /var/www/html/portal
-sudo cp -r dist/insurance-portal/ /var/www/html/portal
-echo "Deployment completed."
-sudo git reset --hard
-echo "Repository reset to last committed state."
-echo "You can access the application at http://your-server-ip/portal"
+cd dist/insurance-portal/
+git init
+git add .
+git commit -m "build"
+git remote set-url origin https://github.com/HussamHassan2/build.git
+git push --set-upstream origin main --force
